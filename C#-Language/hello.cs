8bc3089f07a6ddcbb335bc2e109641ec4f04b970
CsJoy0363 <latestworld9042@gmail.com>
2021-09-29 19:25:48 +0600

    added c# language

---
+++ C#-Language/hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Project_1 {
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("Hello C#");
+            Console.ReadKey ();
+        }
+    }
+}
