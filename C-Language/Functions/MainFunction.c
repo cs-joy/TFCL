acd7bb21f0502b95b240e7f83fbd7b6ab11e07c8
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-20 15:15:56 +0600

    description added

--- C-Language/Functions/MainFunction.c
+++ C-Language/Functions/MainFunction.c
@@ -1,3 +1,15 @@
+/*
+Reference of notes :: https://en.cppreference.com/w/c/language/main_function
+
+# Every C program coded to run in a hosted execution environment contains the definition (not the prototype) of a function 
+  called main, which is the designated start of the program.
+  ----------------------------
+    1. int main(void) {body}
+        2. int main(int argc, char *argv[]) {body}
+            3. another implementation-defined signature (since C99)	
+
+*/
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
