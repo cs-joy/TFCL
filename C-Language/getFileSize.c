2eb19f600d7819ade8615990e5b923b94613c4d3
RTRP <52339481+rtrp-theory@users.noreply.github.com>
2022-03-20 15:50:57 +0600

    Create getFileSize.c
    `stat` Function to Get File Size in C

---
+++ C-Language/getFileSize.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/stat.h>
+
+const char *filename = "input.txt"; // file name - "getFileSize.c"
+
+int main(int argc, char *argv[]) {
+    struct stat sb;
+
+    if (stat(filename, &sb) == -1) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Inode number: %lu\n", sb.st_ino);
+    printf("User ID of owner: %u\n", sb.st_uid);
+    printf("Group ID of owner: %u\n", sb.st_gid);
+    printf("Total file size: %lu bytes\n", sb.st_size);
+    printf("Last status change:       %s", ctime(&sb.st_ctime));
+    printf("Last file access:         %s", ctime(&sb.st_atime));
+    printf("Last file modification:   %s", ctime(&sb.st_mtime));
+
+    exit(EXIT_SUCCESS);
+}
